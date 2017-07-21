0866a716d64fbaefdb8d2199c88687bbfae1c01a
Bill Zimmerlin <zjn700-01@yahoo.com>
2017-07-21 07:56:52 +0000

    add components in develop branch only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { TrashComponent } from './src/app/trash/trash.component';
+import { BranchComponent } from './src/app/branch/branch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    TrashComponent,
+    BranchComponent
   ],
   imports: [
     BrowserModule
