fd901abf142ed0414b36430be69072891504b9cc
Bill Zimmerlin <zjn700-01@yahoo.com>
2017-07-22 03:25:35 +0000

    adding external js file to angular

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+//import { Foo } from '../../assets/js/import-js-test';
+//import * as  Foo from '../../assets/js/import-js-test';
+//declare var sayJello: any;
+//let myFoo = (<any>Foo);
+declare var test: any;
+declare var test2: any;
+declare var Foo: any;
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css', '../shared/bootstrap-3.3.7-dist/css/bootstrap.css']
 })
 export class ShoppingListComponent implements OnInit {
+  private foo;
 
   constructor() { }
 
   ngOnInit() {
+    //sayJello();
+    //myFoo.foo.bar()
+    this.f();
+    this.foo.bar();
+  }
+  
+  f(){
+    new test();
+    new test2();
+    let foo = new Foo();
+    this.foo = foo;
+    foo.bar()
+    
+    
   }
 
 }
---
+++ src/assets/js/import-js-test.js
@@ -0,0 +1,26 @@
+function test(){
+    console.log('TestingFunction')
+}
+
+function test2(){
+    console.log('TestingFunction2')
+  
+}
+
+
+class Foo {
+  bar() {
+    console.log('bar');
+  }
+}
+  
+//   sayJello(){
+//     console.log('jello')
+//   }
+// }
+// exports.Foo = Foo;
+
+
+
+
+//console.log('jello import-js-test')
